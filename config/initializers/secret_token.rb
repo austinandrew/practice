bd1098ad9bee0346621f933193d2a5731a55c894
Austin Zentz <austin.zentz@gmail.com>
2013-12-12 00:35:49 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Walkabout::Application.config.secret_key_base = '291a15c0f3dc3de1b4583e4cee670bb7754fd317f7cb135f1140d065795dd04debf89ebee3e3dbc674699f7c1d05354ed9aded8dd79e09091bd99263befb5505'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Walkabout::Application.config.session_store :cookie_store, key: '_walkabout_session'
